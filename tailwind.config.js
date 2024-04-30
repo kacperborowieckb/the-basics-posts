d3cec0943ed254104f2378f40a4614739ebdf353
kacperborowieckb <kacperborowiec.kb@gmail.com>
2024-04-30 15:27:51 +0200

    test for mdx components

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './Form.tsx',
+    './react-hook-form.mdx',
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
